0b959480c5057208aa6c59a7837341e94cb0cd86
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2025-01-10 14:53:13 +0100

    update readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Julia API Version Differences
+
+[![Julia](https://github.com/tqml/julia-api-collector/actions/workflows/julia.yaml/badge.svg)](https://github.com/tqml/julia-api-collector/actions/workflows/julia.yaml)
+
+This repository contains the code to collect the differences between the APIs of different versions of Julia.
--- main.jl
+++ main.jl
@@ -165,9 +165,9 @@ function extract_functions_from_module(module_)
         end
     end
     method_table = filter(m -> m !== nothing, method_table)
-    
+
     flat_method_table = [m for method in method_table for m in method.ms if m !== nothing]
-    
+
     method_dicts = map(flat_method_table) do m
         temp = method_definition(m)
         tuple_names = (:name, :arg_names, :arg_types, :kwarg_names, :module)
@@ -223,12 +223,11 @@ function extract_functions_from_module(module_)
 end
 
 
-function main() 
+function main()
     for m in (Base, LinearAlgebra, Random, Statistics, Dates, Printf)
         println("Extracting functions from module: ", m)
         extract_functions_from_module(m)
     end
 end
-
 
 main()
